2efd94d3955b7b254cf8b5f7f77b14282969dd0c
Vinicius Ribeiro <viniciusribeirosp@gmail.com>
2017-01-10 07:00:15 -0200

    Added cacheable home

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -24,6 +27,7 @@ import br.com.casadocodigo.loja.models.CarrinhoCompras;
 @EnableWebMvc
 @ComponentScan(basePackageClasses={HomeController.class, ProdutoDAO.class,
 		FileSaver.class, CarrinhoCompras.class})
+@EnableCaching
 public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	
 	@Bean
@@ -69,5 +73,10 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+    
+    @Bean
+    public CacheManager cacheManager(){
+    	return new ConcurrentMapCacheManager();
     }
 }
