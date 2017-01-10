610c5af216af29fdc8bf78dc70f18d6d1b4fe0d6
Vinicius Ribeiro <viniciusribeirosp@gmail.com>
2017-01-10 06:38:48 -0200

    New layout home

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -1,15 +1,27 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.models.Produto;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private ProdutoDAO produtoDao;
+	
 	@RequestMapping("/")
-	public String index(){
-		System.out.println("Entrando na home do CDC");
-		return "home";
+	public ModelAndView index(){
+		List<Produto> produtos = produtoDao.listar();
+		ModelAndView model = new ModelAndView("home");
+		model.addObject("produtos", produtos);
+		return model;
 	}
 
 }
